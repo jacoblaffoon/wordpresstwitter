231926d532d282bc4b445dee29d639a12fa99994
Jacob Laffoon <thomas.j.laffoon@gmail.com>
2020-03-04 15:36:24 -0600

    added twitter-api.php

---
+++ twitter-api.php
@@ -0,0 +1,23 @@
+<?php
+
+/*
+Plugin Name: Twitter API
+Plugin URI: https://consideratecoder.com
+Description: This plugin will fetch your latest tweets and display it using shortcode [tweets]
+Version: 1.0
+Author: Jacob Laffoon
+Author URI: https://consideratecoder.com
+License: GPLv2 or later
+Text Domain: artisansweb
+*/
+
+
+class Twitter_API {
+    public function __construct() {
+ 
+    }
+}
+
+defined( 'ABSPATH' ) or die( 'No script kiddies please!' );
+
+?>
